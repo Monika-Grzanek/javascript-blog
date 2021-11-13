7bf7d4171d60c5052dae21b588c5af5f33529b98
Monika Grzanek <monski@o2.pl>
2021-11-13 21:23:28 +0100

    Add class active to clicked link and article, get href attribute from clicked link and find value of href attribute

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
   }); */
 
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
@@ -16,7 +17,7 @@ const titleClickHandler = function(event){
         console.log(activeLinks);
     }
     /* add class 'active' to the clicked link */
-    clickedElement.classList.add('.active');
+    clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .active');
@@ -25,10 +26,14 @@ const titleClickHandler = function(event){
         console.log(activeArticles);
     }
     /* get 'href' attribute from the clicked link */
-
+    const articleSelector = clickedElement.getAttribute('href')
+    console.log('Link was clicked!', articleSelector);
     /* find the correct article using the selector (value of 'href' attribute) */
-
+    const targetArticle = document.querySelector(articleSelector);
+    console.log('Article selected', targetArticle);
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
+    console.log('Add class active:', targetArticle);
   }
   
 const links = document.querySelectorAll('.titles a');
