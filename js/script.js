c8e43fbba35c7e225e8ed090983ea31a919bebf9
Monika Grzanek <monski@o2.pl>
2021-11-14 13:25:21 +0100

    Add generateTitleLinks with: remove contents of titleList, for each article get the article id, find and get title element

--- js/script.js
+++ js/script.js
@@ -41,3 +41,32 @@ const links = document.querySelectorAll('.titles a');
     for(let link of links){
         link.addEventListener('click', titleClickHandler);
     }
+
+{
+    const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+    function generateTitleLinks(){
+        console.log('Function enabled');
+    /* remove contents of titleList */
+        const titleList = document.querySelector(optTitleListSelector).innerHTML = '';
+    /* for each article */
+        const articles = document.querySelectorAll(optArticleSelector);
+        for(let article of articles){
+            console.log(articles);
+        /* get the article id */
+            const articleId = optArticleSelector.getAttribute('id');
+            console.log('Id downloaded', articleId);
+        /* find the title element */
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+        /* get the title from the title element */
+
+        /* create HTML of the link */
+
+        /* insert link into titleList */
+        }
+    }
+
+    generateTitleLinks();
+}
